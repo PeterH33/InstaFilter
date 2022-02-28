260424458bd49bdf6d263b410297a4bc2bc709fd
Peter Hartnett <peter.hartnett@gmail.com>
2022-02-28 14:36:49 -0500

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Peter Hartnett on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
