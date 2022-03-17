2382ee7bf837c77f70da13af1a2579edbb89e5bc
Peter Hartnett <peter.hartnett@gmail.com>
2022-03-17 16:10:45 -0400

    Commit of the initial files for learning the project, ImagePicker and ImageSaver are both useful wrappers for using the photo library

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -6,13 +6,95 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+
+            Button("Select Image") {
+                showingImagePicker = true
+            }
+            Button("Save Image") {
+                guard let inputImage = inputImage else { return }
+
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+            }
+        }
+        .onChange(of: inputImage) { _ in loadImage() }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $inputImage)
+        }
     }
 }
+
+
+
+//
+//struct ContentView: View {
+//    @State private var image: Image?
+//    @State var amount = 1.0
+//
+//    var body: some View {
+//        VStack {
+//            image?
+//                .resizable()
+//                .scaledToFit()
+//        }
+//        .onAppear(perform: loadImage)
+//    }
+//
+//    //This loadImage() can apply just about any filter, change filter on the noted line
+//    func loadImage() {
+//        guard let inputImage = UIImage(named: "Example") else { return }
+//        let beginImage = CIImage(image: inputImage)
+//
+//        let context = CIContext()
+//
+//        //Change filter here CIFilter.xxxx()
+//        let currentFilter = CIFilter.bloom()
+//        currentFilter.inputImage = beginImage
+//
+////        let amount = 1.0
+//
+//        let inputKeys = currentFilter.inputKeys
+//
+//        if inputKeys.contains(kCIInputIntensityKey) {
+//            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+//        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+//        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+//
+//        // get a CIImage from our filter or exit if that fails
+//        guard let outputImage = currentFilter.outputImage else { return }
+//
+//        // attempt to get a CGImage from our CIImage
+//        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+//            // convert that to a UIImage
+//            let uiImage = UIImage(cgImage: cgimg)
+//
+//            // and convert that to a SwiftUI image
+//            image = Image(uiImage: uiImage)
+//        }
+//
+//    }
+//}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ InstaFilter/ImagePicker.swift
@@ -0,0 +1,54 @@
+//
+//  ImagePicker.swift
+//  InstaFilter
+//
+//  Created by Peter Hartnett on 3/11/22.
+//
+//This is from Paul Hudsons 100 days of swiftui days 63,63. It might be able to simply be dropped into anything to allow for picking images from the users photos library... pretty ugly code that needs to go away
+
+import Foundation
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            // Tell the picker to go away
+            picker.dismiss(animated: true)
+
+            // Exit if no selection was made
+            guard let provider = results.first?.itemProvider else { return }
+
+            // If this has an image we can use, use it
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+        
+        
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self    )
+    }
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+    }
+}
