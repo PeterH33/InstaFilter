1386dbab421837040724235a821d6b537ab973fc
Peter Hartnett <peter.hartnett@gmail.com>
2022-05-05 10:37:38 -0400

    removing some old code and useless imports on ImageSaver

--- InstaFilter/ImageSaver.swift
+++ InstaFilter/ImageSaver.swift
@@ -5,18 +5,6 @@
 //  Created by Peter Hartnett on 3/17/22.
 //
 
-import Foundation
-import SwiftUI
-
-//class ImageSaver: NSObject {
-//    func writeToPhotoAlbum(image: UIImage) {
-//        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
-//    }
-//
-//    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-//        print("Save finished!")
-//    }
-//}
 
 
 import UIKit
